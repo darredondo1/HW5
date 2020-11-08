ce31009f5bdec9fcb41c960034f6cf611bc12c21
slinky57 <slinky57@protonmail.com>
2020-11-08 15:51:21 -0700

    Update scatterAllGather.c

--- scatterAllGather.c
+++ scatterAllGather.c
@@ -22,7 +22,9 @@ int main(int argc, char* argv[])
     char fPath[40];
     sprintf(fPath,"Problem3/ScatterAllGather/nprocs_%d/ScatterAllGather_N_%d.txt",num_procs,N);
     int numDoubles = 1 << N;
+    int blockSize = (int) (numDoubles / num_procs);
     double* send_message = (double*)malloc(numDoubles*sizeof(double));
+    double* last_message = (double*)malloc(blockSize*sizeof(double));
     
     //create vector of 2^N random values
     if (rank==0)
@@ -33,41 +35,26 @@ int main(int argc, char* argv[])
         }
     }
     
-    int nk = (int) log2(num_procs);
-    
-    double start, time;
     for (int n=0;n<numTests;n++)
     {
+        double start, time;
         MPI_Barrier(MPI_COMM_WORLD);
         start = MPI_Wtime();
-        for (int k=1;k<=nk;k++) //NUM STEPS NEEDED TO BROADCAST TO ALL NODES
+        
+        MPI_Scatter(send_message,blockSize,MPI_DOUBLE,last_message,MPI_DOUBLE,0,MPI_COMM_WORLD);
+        
+        send_message[rank*blockSize] = last_message;
+        
+        for (int k=0;k<num_procs;k++)
         {
-            int  num_sources = pow(2,k-1); //NUM PROCS AT STEP K THAT HAVE DATA
-            int spacing = pow(2,nk-k+1);   //SPACING BETWEEN SOURCES
-            int newspacing = pow(2,nk-k);  //HOW FAR TO SEND DATA
-            MPI_Status send_status, recv_status;
-            MPI_Request send_request, recv_request;
-            int send = 0;
-            int recv = 0;
-            for (int i=0;i<num_sources;i++)
-            {
-                if (rank==i*spacing) //SENDERS
-                {
-                    send=1;
-                    MPI_Isend(send_message, numDoubles, MPI_DOUBLE, rank+newspacing,k,MPI_COMM_WORLD,&send_request);
-                }
-                else if (rank==i*spacing+newspacing) //RECEIVERS
-                {
-                    recv=1;
-                    MPI_Irecv(send_message, numDoubles, MPI_DOUBLE,
-                              rank-newspacing,k,MPI_COMM_WORLD,&recv_request);
-                }
-            }
-            if (send==1)
-                MPI_Wait(&send_request,&send_status);
-            if (recv==1)
-                MPI_Wait(&recv_request,&recv_status);
+            MPI_Isend(last_message,blockSize,MPI_DOUBLE,(rank+1)%num_procs,k,MPI_COMM_WORLD,&send_request);
+            MPI_Irecv(last_message,blockSize,MPI_DOUBLE,(rank-1)%num_procs,k,MPI_COMM_WORLD,&recv_request);
+            int idx = (int) ((rank+k)*blockSize)%num_procs
+            send_message[idx]=last_message;
+            MPI_Wait(&send_request,&send_status);
+            MPI_Wait(&recv_request,&recv_status);
         }
+
         MPI_Barrier(MPI_COMM_WORLD);
         time =  (MPI_Wtime() - start);
 
