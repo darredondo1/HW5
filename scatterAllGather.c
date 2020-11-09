3b685c2b079f0fc4802f21eb1a1406a4be4a1dfd
slinky57 <slinky57@protonmail.com>
2020-11-08 19:45:54 -0700

    Update scatterAllGather.c

--- scatterAllGather.c
+++ scatterAllGather.c
@@ -68,7 +68,7 @@ int main(int argc, char* argv[])
         MPI_Barrier(MPI_COMM_WORLD);
         time =  (MPI_Wtime() - start);
 
-        if (rank==(num_procs-1))
+        if (rank==(15))
         {
             double sum2 = 0;
             for (int i=0;i<numDoubles;n++)
